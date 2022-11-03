1e397295dd38fc9e40f25c3e6161a4a6d0270000
hoanga4dc <115219822+hoanga4dc@users.noreply.github.com>
2022-11-03 20:15:47 +0700

    Add files via upload

---
+++ Project 1+2+3/Controller.cpp
@@ -0,0 +1,201 @@
+#pragma once
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+#include<dos.h>
+#include"UI.cpp"
+#include"CheckWin.cpp"
+#include<memory>
+#include"Player.cpp"
+#include"DataProcess.cpp"
+#include"Match_Replay.cpp"
+class Controller {
+private:
+	shared_ptr<UI> u;
+	CheckWin Check;
+public:
+	void Register() {
+		DataProcess DP;
+		shared_ptr<Player> p1(new Player);
+
+		p1->set_Name(u->Register_Acount());
+		p1->set_NumberWin(0);
+		p1->set_NumberLose(0);
+		p1->set_NumberDraw(0);
+		p1->set_WinRate(0);
+		DP.Inser_DataPlayer(p1);
+		cout << "Dang ky thanh cong! ^_^\n";
+	}
+	void RunMatch() {
+		/*DataProcess DP1;
+		
+		vector<shared_ptr<Player>> v1=DP1.get_ListPlayer();
+		cout << v1.size() << endl;
+		for (int i = 0; i < v1.size(); i++) { v1[i]->Diplay_Player(); }*/
+		string player1 = u->Input_NamePlayer1();
+		string player2 = u->Input_NamePlayer2();
+		int x = 0;
+		int y = 0;
+		vector<int> temp;
+		int a[10][10];
+		string TimeMatch = u->Input_TimeMatch();
+		u->Display_Map(a);
+		int d1 = 0;
+		
+		while (d1==0) {
+			int d = 0;
+			u->Input_Xxy(a, x, y, player1);
+			temp.push_back(x);
+			temp.push_back(y);
+			d++;
+			system("cls"); 
+			u->Display_Map(a);
+			if (Check.Check_Win(a, x-1, y-1)) {
+				
+				DataProcess DP;
+				shared_ptr<Player> p1(new Player);
+				p1->set_Name(player1);
+				p1->set_NumberWin(1);
+				p1->set_NumberLose(0);
+				p1->set_NumberDraw(0);
+				p1->set_WinRate(1);
+				DP.Inser_DataPlayer(p1);
+
+				shared_ptr<Player> p2(new Player);
+				p2->set_Name(player2);
+				p2->set_NumberWin(0);
+				p2->set_NumberLose(1);
+				p2->set_NumberDraw(0);
+				p2->set_WinRate(0);
+				DP.Inser_DataPlayer(p2);
+
+				u->Notification_Win(player1);
+				d1++;
+				break;
+			}
+			u->Input_Oxy(a, x, y, player2); d++;
+			temp.push_back(x);
+			temp.push_back(y);
+			system("cls");
+			u->Display_Map(a);
+			if (Check.Check_Win(a, x-1, y-1)) {
+				DataProcess DP2;
+				shared_ptr<Player> p1(new Player);
+				p1->set_Name(player2);
+				p1->set_NumberWin(1);
+				p1->set_NumberLose(0);
+				p1->set_NumberDraw(0);
+				p1->set_WinRate(1);
+				DP2.Inser_DataPlayer(p1);
+
+				shared_ptr<Player> p2(new Player);
+				p2->set_Name(player1);
+				p2->set_NumberWin(0);
+				p2->set_NumberLose(1);
+				p2->set_NumberDraw(0);
+				p2->set_WinRate(0);
+				DP2.Inser_DataPlayer(p2);
+
+
+				u->Notification_Win(player2);
+				d1++;
+				break;
+			}
+			if (d == 100) {
+				DataProcess DP2;
+				shared_ptr<Player> p1(new Player);
+				p1->set_Name(player2);
+				p1->set_NumberWin(0);
+				p1->set_NumberLose(0);
+				p1->set_NumberDraw(1);
+				p1->set_WinRate(0);
+				DP2.Inser_DataPlayer(p1);
+
+				shared_ptr<Player> p2(new Player);
+				p2->set_Name(player1);
+				p2->set_NumberWin(0);
+				p2->set_NumberLose(0);
+				p2->set_NumberDraw(1);
+				p2->set_WinRate(0);
+				DP2.Inser_DataPlayer(p2);
+				u->Notification_Draw();
+				d1++;
+				break;
+			}
+		}
+		shared_ptr<Match_Replay> M(new Match_Replay);
+		M->set_Player1(player1);
+		M->set_Player2(player2);
+		M->set_TimeMatch(TimeMatch);
+		M->set_Step(temp);
+		/*cout << M->get_Player1() << endl;
+		cout << M->get_Player2() << endl;*/
+		DataProcess D;
+		D.Insert_MatchReplay(M);
+	}
+	void DisplayPlayers() {
+		cout << "Danh sach cac nguoi choi:\n";
+		vector<shared_ptr<Player>> v;
+		shared_ptr<DataProcess> DP(new DataProcess);
+		v = DP->get_ListPlayer();
+		for (int i = 0; i < v.size(); i++) {
+			cout << i << "." << v[i]->get_Name() << endl;
+		}
+		cout << "Choose one of list!";
+		int k = 0;
+		cin >> k;
+		v[k]->Diplay_Player();
+		for (int i = 0; i < v.size(); i++) {
+			if (i != k) {
+				if ((v[i]->get_WinRate()==v[k]->get_WinRate())||((v[i]->get_WinRate() < (v[k]->get_WinRate() * 1.1)) && ((v[i]->get_WinRate() > (v[k]->get_WinRate() * 0.9))))) {
+					cout << "nguoi choi co ty le thang gan tuong duong:\n";
+					v[i]->Diplay_Player();
+				}
+			}
+		}
+	}
+	void Display_Relays() {
+		DataProcess DP;
+		vector<shared_ptr<Match_Replay>> v=DP.get_ListMatchReplays();
+		for (int i = 0; i < v.size(); i++) {
+			cout << i << ".\n";
+			v[i]->Display();
+			cout << "-----------------\n";
+		}
+		cout << "choose one match to view^^";
+		int k = 0;
+		cin >> k;
+		cin.ignore();
+		vector<int> step;
+		step = v[k]->get_Step();
+		/*cout << step.size();
+		for (int i = 0; i < step.size(); i++) { cout << step[i] << " "; }*/
+		int k2 = 1;
+		int d = 0;
+		int a[10][10];
+		while (d < step.size()-2) {
+			if (k2==1)
+			{
+				a[step[d] - 1][step[d + 1] - 1] = 1;
+				k2 = 2;
+			}
+
+			else if(k2 == 2) { 
+				a[step[d] - 1][step[d + 1] - 1] = 2; 
+				k2 = 1; }
+			system("cls");
+			if (k2 == 2) { cout << v[k]->get_Player1() << " turn\n"; }
+			if (k2 == 1) { cout << v[k]->get_Player2() << " turn\n"; }
+			u->Display_Map(a);
+			Sleep(600);
+			d += 2;
+		}
+		if (k2 == 2) { cout << v[k]->get_Player1() << " Win\n"; }
+		if (k2 == 1) { cout << v[k]->get_Player2() << " Win\n"; }
+
+	}
+	
+
+};
---
+++ Project 1+2+3/DataProcess.cpp
@@ -0,0 +1,160 @@
+#pragma once
+#include<iostream>
+#include"Match_Replay.cpp"
+#include"Player.cpp"
+#include<fstream>
+#include<memory>
+#include<string>
+#include<vector>
+#include<sstream>
+using namespace std;
+class DataProcess {
+public:
+	DataProcess(){}
+	vector<shared_ptr<Player>> get_ListPlayer() {
+		vector<shared_ptr<Player>> v;
+		ifstream ifs("D:/Code C++/MOCK PROJECT/ConsoleApplication1/ConsoleApplication1/Player.txt");
+		while (!ifs.eof()) {
+			shared_ptr<Player> p(new Player);
+			char temp[255];
+			int x = 0;
+			double y = 0;
+
+			ifs.getline(temp, 255); string s = temp;
+			p->set_Name(s);
+
+			ifs.getline(temp, 255);
+			x = atoi(temp);
+			/*stringstream s1n(s);
+			s1n >> x;*/
+			p->set_NumberWin(x);
+
+			ifs.getline(temp, 255); s = temp;
+			stringstream s2n(s);
+			s2n >> x;
+			p->set_NumberLose(x);
+
+			ifs.getline(temp, 255); s = temp;
+			stringstream s3n(s);
+			s3n >> x;
+			p->set_NumberDraw(x);
+
+			ifs.getline(temp, 255); s = temp;
+			stringstream s4n(s);
+			s4n >> y;
+			p->set_WinRate(y);
+			if ((p->get_NumberWin() != 0) || (p->get_NumberLose() != 0) || (p->get_NumberDraw() != 0)) { v.push_back(p); }
+		}
+		ifs.close();
+		return v;
+	}
+	void Inser_DataPlayer(shared_ptr<Player> p) {
+
+		vector<shared_ptr<Player>> v = get_ListPlayer();
+		//v.push_back(p);
+		if (v.size() == 0) { v.push_back(p); }
+			if (v.size() > 0) {
+			int d = 0;
+				for (int i = 0; i < v.size(); i++) {
+					if (v[i]->get_Name() == p->get_Name()) {
+						d++;
+						v[i]->set_NumberWin(v[i]->get_NumberWin() + p->get_NumberWin());
+						v[i]->set_NumberLose(v[i]->get_NumberLose() + p->get_NumberLose());
+						v[i]->set_NumberDraw(v[i]->get_NumberDraw() + p->get_NumberDraw());
+						v[i]->set_WinRate(double(v[i]->get_NumberWin()) / double((v[i]->get_NumberWin() + v[i]->get_NumberLose() + v[i]->get_NumberDraw())));
+					}
+				}
+			if (d == 0) { v.push_back(p); }
+			}
+		
+		ofstream ofs("D:/Code C++/MOCK PROJECT/ConsoleApplication1/ConsoleApplication1/Player.txt",ostream::out);
+		for (int i = 0; i < v.size(); i++) {
+			
+					ofs << v[i]->get_Name() << endl;
+					ofs << v[i]->get_NumberWin() << endl;
+					ofs << v[i]->get_NumberLose() << endl;
+					ofs << v[i]->get_NumberDraw() << endl;
+					ofs << v[i]->get_WinRate() << endl;
+					
+		
+		}
+		ofs.close();
+	}
+
+	void Insert_MatchReplay(shared_ptr<Match_Replay> p) {
+		ofstream ofs("D:/Code C++/MOCK PROJECT/ConsoleApplication1/ConsoleApplication1/Replays.txt", ostream::app);
+		if (ofs.fail()) { cout << "failtowite!"; }
+		ofs << p->get_Player1() << endl;
+		ofs << p->get_Player2() << endl;
+		ofs << p->get_TimerMatch() << endl;
+		for (int i = 0; i <(p->get_Step().size()); i++) {
+			ofs << p->get_Step()[i] << " ";
+		}
+		ofs << "$" << endl;
+		ofs.close();
+	}
+	vector<int> get_ListStep(string s) {
+		vector<int> v;
+		int d = 0;
+		string temp="";
+		while (d < s.size()) {
+			if (s[d] != ' ') {
+				temp += s[d];
+			}
+			if ((s[d] == ' ')||(s[d]=='$')) {
+				int x = 0;
+				stringstream s2n(temp);
+				s2n >> x;
+				v.push_back(x);
+				temp = "";
+			}
+			d++;
+
+		}
+		return v;
+	}
+	vector<shared_ptr<Match_Replay>> get_ListMatchReplays() {
+		vector<shared_ptr<Match_Replay>> v;
+		ifstream ifs("D:/Code C++/MOCK PROJECT/ConsoleApplication1/ConsoleApplication1/Replays.txt");
+		while (!ifs.eof()) {
+			shared_ptr<Match_Replay> p(new Match_Replay);
+			char temp[500];
+			ifs.getline(temp, 500);
+			string s = temp;
+			p->set_Player1(s);
+
+			ifs.getline(temp, 500);
+			s = temp;
+			p->set_Player2(s);
+
+			ifs.getline(temp, 500); s = temp;
+			p->set_TimeMatch(s);
+
+			ifs.getline(temp, 500); s = temp;
+			p->set_Step(get_ListStep(s));
+			if ((p->get_Player1() != "") && (p->get_TimerMatch() != "")) { v.push_back(p); }
+		}
+		ifs.close();
+		return v;
+	}
+	/*bool Check_ExistPlayer(shared_ptr<Player> p) {
+		vector<shared_ptr<Player>> v;
+		v = get_ListPlayer();
+		for (int i = 0; i < v.size(); i++) {
+			if (v[i]->get_Name() == p->get_Name()) {
+				return false;
+			}
+		}
+		return true;
+	}*/
+	bool Check_ExistAcount(string s) {
+		vector<shared_ptr<Player>> v;
+		v = get_ListPlayer();
+		for (int i = 0; i < v.size(); i++) {
+			if (v[i]->get_Name() == s) {
+				return true;
+			}
+		}
+		return false;
+	}
+};
